ed0de76dd820f258ab2c4bdc1b5f4420f8104d69
Jose Camarena <jose.j.camarena@gmail.com>
2023-07-01 09:45:25 -0700

    Add MemoryRepository, MemoryService

--- src/main/java/com/example/backendsvc/data/repositories/MemoryRepository.java
+++ src/main/java/com/example/backendsvc/data/repositories/MemoryRepository.java
@@ -1,4 +1,9 @@
 package com.example.backendsvc.data.repositories;
 
-public interface MemoryRepository {
+import com.example.backendsvc.domain.Memory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemoryRepository extends JpaRepository<Memory, Long> {
 }
---
+++ src/main/java/com/example/backendsvc/services/MemoryService.java
@@ -0,0 +1,37 @@
+package com.example.backendsvc.services;
+
+import com.example.backendsvc.data.repositories.MemoryRepository;
+import com.example.backendsvc.domain.Memory;
+import jakarta.transaction.Transactional;
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@Getter
+@Setter
+@Builder
+public class MemoryService {
+    private MemoryRepository memoryRepository;
+    public MemoryService(MemoryRepository memoryRespository){
+        this.memoryRepository = memoryRespository;
+    }
+    public void deleteAllMemories(){
+        memoryRepository.deleteAll();
+    }
+    public List<Memory> findAllMemories(){
+        return memoryRepository.findAll();
+    }
+    public Optional<Memory> findMemoryById(Long id){
+        return memoryRepository.findById(id);
+    }
+    public Memory saveMemory(Memory memory){
+        return memoryRepository.save(memory);
+    }
+    public void deleteMemoryById(Long id){
+        memoryRepository.deleteById(id);
+    }
+}
